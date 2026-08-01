409960fa6fc0a3421ec933c839615ffaadc711cb
NISHANTH-777 <nisanthp,cs24@bitsathy.ac.in>
2026-08-01 15:57:55 +0530

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
